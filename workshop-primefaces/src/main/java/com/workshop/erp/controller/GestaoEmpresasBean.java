694da5838821b84f039e3d49b885501098fd5f55
Raphael Barreto <tnt.raphael@gmail.com>
2016-03-26 18:27:53 -0300

    Selecionando linhas da tabela de dados

--- workshop-primefaces/src/main/java/com/workshop/erp/controller/GestaoEmpresasBean.java
+++ workshop-primefaces/src/main/java/com/workshop/erp/controller/GestaoEmpresasBean.java
@@ -33,6 +33,7 @@ public class GestaoEmpresasBean implements Serializable {
 
 	private List<Empresa> todasEmpresas;
 	private Empresa empresaEdicao = new Empresa();
+	private Empresa empresaSelecionada;
 
 	public void prepararNovoCadastro() {
 		empresaEdicao = new Empresa();
@@ -67,4 +68,11 @@ public class GestaoEmpresasBean implements Serializable {
 		this.empresaEdicao = empresaEdicao;
 	}
 
+	public Empresa getEmpresaSelecionada() {
+		return empresaSelecionada;
+	}
+
+	public void setEmpresaSelecionada(Empresa empresaSelecionada) {
+		this.empresaSelecionada = empresaSelecionada;
+	}
 }
