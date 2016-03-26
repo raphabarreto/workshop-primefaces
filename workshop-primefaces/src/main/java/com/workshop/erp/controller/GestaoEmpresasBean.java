33423e6b76580a9e446671e66155eaea5533605a
Raphael Barreto <tnt.raphael@gmail.com>
2016-03-26 15:50:12 -0300

    Implementando o cadastro de empresa

--- workshop-primefaces/src/main/java/com/workshop/erp/controller/GestaoEmpresasBean.java
+++ workshop-primefaces/src/main/java/com/workshop/erp/controller/GestaoEmpresasBean.java
@@ -8,7 +8,10 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.workshop.erp.model.Empresa;
+import com.workshop.erp.model.TipoEmpresa;
+import com.workshop.erp.model.service.CadastroEmpresaService;
 import com.workshop.erp.repository.Empresas;
+import com.workshop.erp.util.FacesMessages;
 
 @Named
 @ViewScoped
@@ -19,7 +22,25 @@ public class GestaoEmpresasBean implements Serializable {
 	@Inject
 	private Empresas empresas;
 
+	@Inject
+	private CadastroEmpresaService cadastroEmpresa;
+
+	@Inject
+	private FacesMessages messages;
+
 	private List<Empresa> todasEmpresas;
+	private Empresa empresaEdicao = new Empresa();
+
+	public void prepararNovoCadastro() {
+		empresaEdicao = new Empresa();
+	}
+
+	public void salvar() {
+		cadastroEmpresa.salvar(empresaEdicao);
+		consultar();
+
+		messages.info("Empresa salva com sucesso!");
+	}
 
 	public void consultar() {
 		todasEmpresas = empresas.todas();
@@ -27,6 +48,18 @@ public class GestaoEmpresasBean implements Serializable {
 
 	public List<Empresa> getTodasEmpresas() {
 		return todasEmpresas;
+	}
+
+	public TipoEmpresa[] getTiposEmpresas() {
+		return TipoEmpresa.values();
+	}
+
+	public Empresa getEmpresaEdicao() {
+		return empresaEdicao;
+	}
+
+	public void setEmpresaEdicao(Empresa empresaEdicao) {
+		this.empresaEdicao = empresaEdicao;
 	}
 
 }
